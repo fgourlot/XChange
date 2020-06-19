e44771bd1afc9ba46472637938a916b3c1c44730
cymp <nguyen.cyril@gmail.com>
2020-06-19 11:24:02 +0200

    orderBookSynchroTest commented and ready

--- xchange-stream-bittrex/src/test/java/info/bitrich/xchangesrteam/bittrex/BittrexManualExample.java
+++ xchange-stream-bittrex/src/test/java/info/bitrich/xchangesrteam/bittrex/BittrexManualExample.java
@@ -3,24 +3,38 @@ package info.bitrich.xchangesrteam.bittrex;
 import info.bitrich.xchangestream.bittrex.BittrexStreamingExchange;
 import info.bitrich.xchangestream.core.StreamingExchange;
 import info.bitrich.xchangestream.core.StreamingExchangeFactory;
+import io.reactivex.disposables.Disposable;
 
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.knowm.xchange.ExchangeSpecification;
+import org.knowm.xchange.bittrex.dto.marketdata.BittrexDepthV3;
 import org.knowm.xchange.bittrex.service.BittrexAccountService;
+import org.knowm.xchange.bittrex.service.BittrexMarketDataService;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.marketdata.OrderBook;
 import org.knowm.xchange.service.account.AccountService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class BittrexManualExample {
 
   private static final Logger LOG = LoggerFactory.getLogger(BittrexManualExample.class);
-
-
 
   public static void main(String[] args) throws IOException {
     ExchangeSpecification exchangeSpecification =
@@ -39,13 +53,16 @@ public class BittrexManualExample {
             });
   }
 
-  ExchangeSpecification exchangeSpecification;
-  String apiKey = "";
-  String apiSecret = "";
-  StreamingExchange exchange;
+  static ExchangeSpecification exchangeSpecification;
+  static String apiKey = "";
+  static String apiSecret = "";
+  static CurrencyPair market = CurrencyPair.ETH_BTC;
+  static StreamingExchange exchange;
+  static Optional<Timer> timer;
 
-  @Before
-  public void init(){
+  @BeforeClass
+  public static void setup() {
+    market = CurrencyPair.ETH_BTC;
     exchangeSpecification = new ExchangeSpecification(BittrexStreamingExchange.class.getName());
     exchangeSpecification.setApiKey("78cf1e6997c4443b88729a5de3b60174");
     exchangeSpecification.setSecretKey("8930cfe72ae7429c8d8fa5bb69b2958c");
@@ -68,5 +85,80 @@ public class BittrexManualExample {
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
+  }
+
+  /**
+   * Compares what is retrieved via websocket with periodic rest gets
+   * - first subscribes via WS and fill a map with the WS books as values, and book sequence as the key
+   * - then launches a timer and fill a map with the rest books as values, and book sequence as the key
+   * - then checks that the books from the rest map are in the WS map.
+   */
+  @Test
+  public void orderBookSynchroTest() {
+    // Maps to compare
+    Map<String, OrderBook> bookMapWS = new ConcurrentHashMap<>();
+    Map<String, OrderBook> bookMapRest = new ConcurrentHashMap<>();
+
+    // WS orderbook map filling
+    AtomicBoolean canStartRestFilling = new AtomicBoolean();
+    Disposable wsDisposable = exchange
+        .getStreamingMarketDataService()
+        .getOrderBook(market)
+        .subscribe(
+            orderBook -> {
+              LOG.debug("Received order book {}", orderBook);
+              bookMapWS.put(orderBook.getMetadata().get(BittrexDepthV3.SEQUENCE).toString(), orderBook);
+              canStartRestFilling.set(true);
+            });
+
+    // Timed Rest orderbook map filling, 5s period
+    timer = Optional.of(new Timer());
+    timer.get().scheduleAtFixedRate(new TimerTask() {
+                                      public void run() {
+                                        if (canStartRestFilling.get()) {
+                                          try {
+                                            Pair<OrderBook, String> orderBookV3 = ((BittrexMarketDataService) exchange.getMarketDataService()).getOrderBookV3(market);
+                                            bookMapRest.put(orderBookV3.getRight(), orderBookV3.getLeft());
+                                          } catch (IOException e) {
+                                            LOG.error("Error rest-getting the orderbook", e);
+                                          }
+                                        }
+                                      }
+                                    },
+                                    0,
+                                    TimeUnit.SECONDS.toMillis(5));
+
+    // Let it run for 20_000ms
+    try {
+      Thread.sleep(20_000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } finally {
+      // Stopping everyone
+      wsDisposable.dispose();
+      timer.ifPresent(Timer::cancel);
+    }
+
+    // Check the books are equal
+    bookMapRest.entrySet().stream()
+               // We discard the REST books outside of the WS running period, in case the REST filling started or ended outside of the WS 
+               // connection window
+               .filter(restBookEntry -> bookMapWS.keySet().stream()
+                                                 .anyMatch(wsSeq -> {
+                                                   long restSeqLong = Long.parseLong(restBookEntry.getKey());
+                                                   long wsSeqLong = Long.parseLong(wsSeq);
+                                                   return restSeqLong <= wsSeqLong && wsSeqLong <= restSeqLong;
+                                                 }))
+               .forEach(restBookEntry -> {
+                 OrderBook orderBookWS = bookMapWS.get(restBookEntry.getKey());
+                 Assert.assertNotNull(orderBookWS);
+                 // using OrderBook.ordersEqual to prevent from comparing the timestamps
+                 Assert.assertTrue(orderBookWS.ordersEqual(restBookEntry.getValue()));
+               });
+  }
+
+  @AfterClass
+  public static void dispose() {
+    timer.ifPresent(Timer::cancel);
   }
 }
