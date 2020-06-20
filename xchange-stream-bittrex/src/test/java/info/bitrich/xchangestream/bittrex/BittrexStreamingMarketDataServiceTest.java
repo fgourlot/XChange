c12504ecd03daa47eb5319681fba57ca515d07ce
cymp <nguyen.cyril@gmail.com>
2020-06-20 16:14:50 +0200

    comments

--- xchange-stream-bittrex/src/test/java/info/bitrich/xchangestream/bittrex/BittrexStreamingMarketDataServiceTest.java
+++ xchange-stream-bittrex/src/test/java/info/bitrich/xchangestream/bittrex/BittrexStreamingMarketDataServiceTest.java
@@ -22,19 +22,20 @@ public class BittrexStreamingMarketDataServiceTest extends TestCase {
     CurrencyPair market = CurrencyPair.ETH_BTC;
     String sequence = "1234";
 
+    // Orderbook to update
     List<LimitOrder> bookBids = new ArrayList<>();
     List<LimitOrder> bookAsks = new ArrayList<>();
 
-    LimitOrder bid1 = new LimitOrder(Order.OrderType.BID, new BigDecimal("1"), market, null, null, new BigDecimal("5"));
-    LimitOrder bid2 = new LimitOrder(Order.OrderType.BID, new BigDecimal("2"), market, null, null, new BigDecimal("4"));
-    LimitOrder bid3 = new LimitOrder(Order.OrderType.BID, new BigDecimal("3"), market, null, null, new BigDecimal("3"));
+    LimitOrder bid1 = new LimitOrder(Order.OrderType.BID, new BigDecimal("1"), market, "", null, new BigDecimal("5"));
+    LimitOrder bid2 = new LimitOrder(Order.OrderType.BID, new BigDecimal("2"), market, "", null, new BigDecimal("4"));
+    LimitOrder bid3 = new LimitOrder(Order.OrderType.BID, new BigDecimal("3"), market, "", null, new BigDecimal("3"));
     bookBids.add(bid1);
     bookBids.add(bid2);
     bookBids.add(bid3);
 
-    LimitOrder ask1 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("1"), market, null, null, new BigDecimal("6"));
-    LimitOrder ask2 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("2"), market, null, null, new BigDecimal("7"));
-    LimitOrder ask3 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("3"), market, null, null, new BigDecimal("8"));
+    LimitOrder ask1 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("1"), market, "", null, new BigDecimal("6"));
+    LimitOrder ask2 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("2"), market, "", null, new BigDecimal("7"));
+    LimitOrder ask3 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("3"), market, "", null, new BigDecimal("8"));
     bookAsks.add(ask1);
     bookAsks.add(ask2);
     bookAsks.add(ask3);
@@ -43,7 +44,7 @@ public class BittrexStreamingMarketDataServiceTest extends TestCase {
     Map<String, Object> metadata = Map.of(BittrexDepthV3.SEQUENCE, sequence);
     orderBook.setMetadata(metadata);
 
-
+    // delta entries to apply to the orderbook to update
     BittrexOrderBookEntry deleteEntry = new BittrexOrderBookEntry(BigDecimal.ZERO, new BigDecimal("4"));
     BittrexOrderBookEntry addEntry = new BittrexOrderBookEntry(new BigDecimal("1.1"), new BigDecimal("9"));
 
@@ -52,21 +53,22 @@ public class BittrexStreamingMarketDataServiceTest extends TestCase {
 
     BittrexOrderBook bittrexOrderBook = new BittrexOrderBook("ETH-BTC", 500, Integer.parseInt(sequence), deltaAsksEntries, deltaBidsEntries);
 
+    // apply the updates
     OrderBook orderBookUpdated = BittrexStreamingMarketDataService.updateOrderBook(orderBook, bittrexOrderBook);
 
-    // Check
+    // Expected Orderbook
     List<LimitOrder> updatedBookBids = new ArrayList<>();
     List<LimitOrder> updatedBookAsks = new ArrayList<>();
 
-    LimitOrder expectedbid1 = new LimitOrder(Order.OrderType.BID, new BigDecimal("1"), market, null, null, new BigDecimal("5"));
-    LimitOrder expectedbid2 = new LimitOrder(Order.OrderType.BID, new BigDecimal("3"), market, null, null, new BigDecimal("3"));
+    LimitOrder expectedbid1 = new LimitOrder(Order.OrderType.BID, new BigDecimal("1"), market, "", null, new BigDecimal("5"));
+    LimitOrder expectedbid2 = new LimitOrder(Order.OrderType.BID, new BigDecimal("3"), market, "", null, new BigDecimal("3"));
     updatedBookBids.add(expectedbid1);
     updatedBookBids.add(expectedbid2);
 
-    LimitOrder expectedask1 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("1"), market, null, null, new BigDecimal("6"));
-    LimitOrder expectedask2 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("2"), market, null, null, new BigDecimal("7"));
-    LimitOrder expectedask3 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("3"), market, null, null, new BigDecimal("8"));
-    LimitOrder expectedask4 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("1.1"), market, null, null, new BigDecimal("9"));
+    LimitOrder expectedask1 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("1"), market, "", null, new BigDecimal("6"));
+    LimitOrder expectedask2 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("2"), market, "", null, new BigDecimal("7"));
+    LimitOrder expectedask3 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("3"), market, "", null, new BigDecimal("8"));
+    LimitOrder expectedask4 = new LimitOrder(Order.OrderType.ASK, new BigDecimal("1.1"), market, "", null, new BigDecimal("9"));
     updatedBookAsks.add(expectedask1);
     updatedBookAsks.add(expectedask2);
     updatedBookAsks.add(expectedask3);
@@ -74,6 +76,7 @@ public class BittrexStreamingMarketDataServiceTest extends TestCase {
 
     OrderBook expectedUpdatedOrderBook = new OrderBook(null, updatedBookAsks, updatedBookBids, true);
 
+    // Test that the initial orderbook with deltas applied is equal to the expected orderbook
     Assert.assertTrue(orderBookUpdated.ordersEqual(expectedUpdatedOrderBook));
   }
 }
