f8c5ff6d9073fe92897dc7049e220678ecdbbb82
Fabien Gourlot <fabien.gourlot@orange.com>
2020-06-24 14:50:10 +0200

    created streamingTradeService test class

---
+++ xchange-stream-bittrex/src/test/java/info/bitrich/xchangestream/bittrex/BittrexStreamingTradeServiceTest.java
@@ -0,0 +1,11 @@
+package info.bitrich.xchangestream.bittrex;
+
+import org.junit.Test;
+
+public class BittrexStreamingTradeServiceTest extends BittrexStreamingBaseTest{
+
+    @Test
+    public void openOrdersAfterOrderTest() {
+
+    }
+}
