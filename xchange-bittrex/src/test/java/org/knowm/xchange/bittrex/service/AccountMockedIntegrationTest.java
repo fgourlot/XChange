d8e0fd0c317c5680cf94eb987cab88b6c62583fe
cymp <nguyen.cyril@gmail.com>
2020-06-25 19:05:40 +0200

    cleaning

--- xchange-bittrex/src/test/java/org/knowm/xchange/bittrex/service/AccountMockedIntegrationTest.java
+++ xchange-bittrex/src/test/java/org/knowm/xchange/bittrex/service/AccountMockedIntegrationTest.java
@@ -1,30 +1,22 @@
 package org.knowm.xchange.bittrex.service;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.assertj.core.api.Assertions.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.knowm.xchange.dto.account.FundingRecord.Type.DEPOSIT;
 
-import java.io.File;
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Date;
-import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
-import org.knowm.xchange.Exchange;
-import org.knowm.xchange.bittrex.BittrexExchange;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.dto.account.AccountInfo;
 import org.knowm.xchange.dto.account.Balance;
-import org.knowm.xchange.dto.account.FundingRecord;
 import org.knowm.xchange.dto.account.Wallet;
-import org.knowm.xchange.service.trade.params.TradeHistoryParams;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -32,6 +24,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class AccountMockedIntegrationTest extends BaseMockedIntegrationTest {
 
   private static BittrexAccountService accountService;
+  private static final String BALANCES_FILE_NAME = "balances.json";
 
   @Before
   public void setUp() {
@@ -46,27 +39,28 @@ public class AccountMockedIntegrationTest extends BaseMockedIntegrationTest {
                 aResponse()
                     .withStatus(200)
                     .withHeader("Content-Type", "application/json")
-                    .withBodyFile("balances.json")));
+                    .withBodyFile(BALANCES_FILE_NAME)));
     AccountInfo accountInfo = accountService.getAccountInfo();
     assertThat(accountInfo).isNotNull();
 
     Wallet wallet = accountInfo.getWallet();
     assertThat(wallet).isNotNull();
 
-
-
     // What's in the mocked json
     final ObjectMapper mapper = new ObjectMapper();
-    JsonNode jsonRoot = mapper.readTree(this.getClass().getResource("/" + WIREMOCK_FILES_PATH + "/balances.json"));
+    JsonNode jsonRoot =
+        mapper.readTree(
+            this.getClass().getResource("/" + WIREMOCK_FILES_PATH + "/" + BALANCES_FILE_NAME));
     JsonNode jsonBtcBalance = jsonRoot.get(0);
     Currency expectedCurrency = new Currency(jsonBtcBalance.get("currencySymbol").textValue());
     BigDecimal expectedTotal = new BigDecimal(jsonBtcBalance.get("total").textValue());
     BigDecimal expectedAvailable = new BigDecimal(jsonBtcBalance.get("available").textValue());
     BigDecimal expectedFrozen = expectedTotal.subtract(expectedAvailable);
-    Date expectedTimestamp = Date.from(ZonedDateTime.parse(jsonBtcBalance.get("updatedAt").textValue()).toInstant());
+    Date expectedTimestamp =
+        Date.from(ZonedDateTime.parse(jsonBtcBalance.get("updatedAt").textValue()).toInstant());
     int expectedNumberOfBalances = jsonRoot.size();
 
-    Balance btcBalance = wallet.getBalance(Currency.BTC);
+    Balance btcBalance = wallet.getBalance(expectedCurrency);
 
     assertThat(wallet.getBalances().size()).isEqualTo(expectedNumberOfBalances);
     assertThat(btcBalance.getCurrency()).isEqualTo(expectedCurrency);
