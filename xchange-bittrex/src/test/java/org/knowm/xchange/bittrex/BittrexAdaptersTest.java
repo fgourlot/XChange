514fb03c05c3eef28c8269147d79f4c19185c17b
cymp <nguyen.cyril@gmail.com>
2020-06-25 17:25:18 +0200

    accountService integration test added

--- xchange-bittrex/src/test/java/org/knowm/xchange/bittrex/BittrexAdaptersTest.java
+++
@@ -1,5 +0,0 @@
-package org.knowm.xchange.bittrex;
-
-public class BittrexAdaptersTest {
-
-}
--- xchange-bittrex/src/test/java/org/knowm/xchange/bittrex/service/AccountMockedIntegrationTest.java
+++ xchange-bittrex/src/test/java/org/knowm/xchange/bittrex/service/AccountMockedIntegrationTest.java
@@ -2,11 +2,20 @@ package org.knowm.xchange.bittrex.service;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.knowm.xchange.dto.account.FundingRecord.Type.DEPOSIT;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.bittrex.BittrexExchange;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.dto.account.AccountInfo;
 import org.knowm.xchange.dto.account.Balance;
@@ -27,7 +36,7 @@ public class AccountMockedIntegrationTest extends BaseMockedIntegrationTest {
   @Test
   public void accountInfoTest() throws Exception {
     stubFor(
-        get(urlPathEqualTo("/api/v1.1/account/getbalances"))
+        get(urlPathEqualTo("/api/v3/balances"))
             .willReturn(
                 aResponse()
                     .withStatus(200)
@@ -35,36 +44,25 @@ public class AccountMockedIntegrationTest extends BaseMockedIntegrationTest {
                     .withBodyFile("balances.json")));
     AccountInfo accountInfo = accountService.getAccountInfo();
     assertThat(accountInfo).isNotNull();
+
     Wallet wallet = accountInfo.getWallet();
     assertThat(wallet).isNotNull();
-    Balance btcBalance = wallet.getBalance(Currency.BTC);
-    assertThat(btcBalance).isNotNull();
-    assertThat(btcBalance.getAvailable()).isNotNull().isPositive();
-  }
 
-  @Test
-  public void fundingHistoryTest() throws Exception {
-    stubFor(
-        get(urlPathEqualTo("/api/v1.1/account/getdeposithistory"))
-            .willReturn(
-                aResponse()
-                    .withStatus(200)
-                    .withHeader("Content-Type", "application/json")
-                    .withBodyFile("deposithistory.json")));
-    stubFor(
-        get(urlPathEqualTo("/api/v1.1/account/getwithdrawalhistory"))
-            .willReturn(
-                aResponse()
-                    .withStatus(200)
-                    .withHeader("Content-Type", "application/json")
-                    .withBodyFile("withdrawalhistory.json")));
-    TradeHistoryParams params = accountService.createFundingHistoryParams();
-    List<FundingRecord> fundingHistory = accountService.getFundingHistory(params);
-    assertThat(fundingHistory).isNotEmpty();
-    FundingRecord record = fundingHistory.get(0);
-    assertThat(record).isNotNull();
-    assertThat(record.getType()).isEqualTo(DEPOSIT);
-    assertThat(record.getCurrency()).isEqualTo(Currency.BTC);
-    assertThat(record.getAmount()).isNotNull().isPositive();
+    Balance btcBalance = wallet.getBalance(Currency.BTC);
+
+    // What's in the mocked json
+    Currency expectedCurrency = Currency.BTC;
+    BigDecimal expectedTotal = new BigDecimal("5265.89272032");
+    BigDecimal expectedAvailable = new BigDecimal("626.54401024");
+    BigDecimal expectedFrozen = expectedTotal.subtract(expectedAvailable);
+    Date expectedTimestamp = Date.from(ZonedDateTime.parse("2020-06-25T14:38:46.06Z").toInstant());
+    int expectedNumberOfBalances = 3;
+
+    assertThat(wallet.getBalances().size()).isEqualTo(expectedNumberOfBalances);
+    assertThat(btcBalance.getCurrency()).isEqualTo(expectedCurrency);
+    assertThat(btcBalance.getTotal().compareTo(expectedTotal)).isEqualTo(0);
+    assertThat(btcBalance.getAvailable().compareTo(expectedAvailable)).isEqualTo(0);
+    assertThat(btcBalance.getFrozen().compareTo(expectedFrozen)).isEqualTo(0);
+    assertThat(btcBalance.getTimestamp()).isEqualTo(expectedTimestamp);
   }
 }
